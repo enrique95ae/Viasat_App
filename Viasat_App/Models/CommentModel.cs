e798dc8104f7b27bfbde83602a4060440063149d
enrique95ae <enrique95ae@gmail.com>
2019-03-15 13:24:34 -0700

    commentId added

--- Viasat_App/Models/CommentModel.cs
+++ Viasat_App/Models/CommentModel.cs
@@ -4,6 +4,7 @@ namespace Viasat_App
 {
     public class CommentModel
     {
+        public int CommentId { get; set; }
         public string CommentTitle { get; set; }
         public string CommentDescription { get; set; }
         public string CommentDate { get; set; }
