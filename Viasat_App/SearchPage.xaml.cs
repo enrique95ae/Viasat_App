6b5a5a9c63bf21af37a7056acc2670f05afe440c
enrique95ae <enrique95ae@gmail.com>
2019-04-11 10:45:17 -0700

    .

--- Viasat_App/SearchPage.xaml.cs
+++ Viasat_App/SearchPage.xaml.cs
@@ -5,11 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+using System.Net.Http;
+using ItemData;
 
 namespace Viasat_App
 {
     public partial class SearchPage : ContentPage
     {
+        public ObservableCollection<ItemJson> itemsList = new ObservableCollection<ItemJson>();
 
         public List<string> parametersList;
 
@@ -24,32 +28,44 @@ namespace Viasat_App
             parametersList.Add("Description");
 
         }
-        //Populate the list
-
-        //END of Populating initial list
-        //ParameterListView.ItemsSource = ParameterList;
 
         //START: BUTTONS EVENTS #######################################################
 
+        //PURPOSE: upon user click the app sends a request to the API and waits for a response and handles the incoming data accordingly:
+        //PARAMETERS: navigation params
+        //ALGORITHM:
+        //  -Button is clicked
+        //  -API endpoint is created in a string according to the search parameters
+        //  -HTTP client is created
+        //  -App waits for a response in a JSON format
+        //  -Response is checked for errors. If success data is parsed into an object of type: ItemData
         private async void resultsButton_Clicked(object sender, EventArgs e)
         {
+            /*
+             * Logic for building a custom request based on the search parameters selected goes here.
+             * As of right now the search is harcoded so we have a working api connection, data retreival and parsing.
+             * Needs to be implemented in order to make searches dynamic.
+             */
+
+            //setting up endpoint
+            string endpointSt = "http://enriqueae.com/ViasatTest/itemsJson.json";
+            Uri apiUri = new Uri(endpointSt);
+
+            //creating a http client to handle the async data retreival
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(apiUri);
+
+            //response stream into a string
+            string jsonContent = await response.Content.ReadAsStringAsync();
+
+            itemsList = JsonConvert.DeserializeObject<ObservableCollection<ItemJson>>(jsonContent);
+
             await Navigation.PushAsync(new ResultsPage());
         }
+
 
         //END: BUTTONS EVENTS #######################################################
     }
 }
 
-        /* SEARCHING ALGORITHM
-         * 1: User enters a keword(s) in the entry field and selects parameters
-         * 2: Upon click event the parameters and keywords are put into an object
-         * 3: The object is serialized and sent trough the network
-         * 4: Application waits for a response
-         * 5: Once response data is received it is deserialized
-         * 6: Deserialized data is parsed
-         * 7: IF
-         *      -Objects: data is put into objects and these into an ObservableCollection (list)
-         *      -Error/Empty/Not found: [DEFINE HANDLING ACTION]
-         * 8: Object is passed to the next page -> ResultsPage
-         */
 
